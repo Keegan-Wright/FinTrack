25e7fb1be664b45bd76ea43da81e565cade9e24c
Keegan Wright <keeganwright12@gmail.com>
2026-02-03 12:21:52 +0000

    classification service

---
+++ Src/FinanceTracker.Services/Classification/ClassificationService.cs
@@ -0,0 +1,128 @@
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using EFCore.BulkExtensions;
+using FinanceTracker.Data;
+using FinanceTracker.Data.Models;
+using FinanceTracker.Models.Request.Classifications;
+using FinanceTracker.Models.Response.Classifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTracker.Services.Classification;
+
+public class ClassificationService : ServiceBase, IClassificationService
+{
+    public ClassificationService(ClaimsPrincipal user, FinanceTrackerContext financeTrackerContext) : base(user,
+        financeTrackerContext)
+    {
+    }
+
+    public async IAsyncEnumerable<ClassificationsResponse> GetAllCustomClassificationsAsync(
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        var classifications = _financeTrackerContext.IsolateToUser(UserId)
+            .Include(x => x.CustomClassifications)
+            .SelectMany(x => x.CustomClassifications).AsAsyncEnumerable();
+
+        await foreach (var classification in classifications.WithCancellation(cancellationToken))
+            yield return new ClassificationsResponse
+            {
+                Tag = classification.Tag,
+                ClassificationId = classification.Id
+            };
+    }
+
+    public async Task<GetClassificationResponse> GetClassificationAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var classification = await _financeTrackerContext.IsolateToUser(UserId)
+            .Include(x => x.CustomClassifications)
+            .SelectMany(x => x.CustomClassifications)
+            .SingleAsync(x => x.Id == id, cancellationToken);
+
+        return new GetClassificationResponse
+        {
+            Tag = classification.Tag,
+            ClassificationId = classification.Id
+        };
+    }
+
+    public async Task<ClassificationsResponse> AddCustomClassificationAsync(AddClassificationsRequest classification,
+        CancellationToken cancellationToken)
+    {
+        var newClassification = new CustomClassification { Tag = classification.Tag };
+
+        var user = await _financeTrackerContext.IsolateToUser(UserId).Include(x => x.CustomClassifications)
+            .SingleAsync(cancellationToken);
+
+        user.CustomClassifications.Add(newClassification);
+
+        await _financeTrackerContext.SaveChangesAsync(cancellationToken);
+
+        return new ClassificationsResponse
+        {
+            Tag = classification.Tag,
+            ClassificationId = newClassification.Id
+        };
+    }
+
+    public async Task AddCustomClassificationsToTransactionAsync(
+        AddCustomClassificationsToTransactionRequest requestModel,
+        CancellationToken cancellationToken)
+    {
+        var query = _financeTrackerContext.IsolateToUser(UserId);
+
+
+        var transaction = await query
+            .Include(x => x.Providers).ThenInclude(x => x.Accounts).ThenInclude(x => x.Transactions)
+            .ThenInclude(x => x.Classifications)
+            .SelectMany(x => x.Providers.SelectMany(c => c.Accounts).SelectMany(r => r.Transactions))
+            .FirstOrDefaultAsync(x => x.Id == requestModel.TransactionId, cancellationToken);
+
+        var classifications = query
+            .Include(x => x.CustomClassifications)
+            .SelectMany(x => x.CustomClassifications)
+            .Where(x => requestModel.Classifications.Select(c => c.ClassificationId).Contains(x.Id))
+            .ToAsyncEnumerable();
+
+        var newClassifications = new List<OpenBankingTransactionClassifications>();
+        await foreach (var classification in classifications.WithCancellation(cancellationToken))
+        {
+            var newClassification = new OpenBankingTransactionClassifications
+            {
+                Transaction = transaction,
+                TransactionId = transaction.Id,
+                Classification = classification.Tag,
+                IsCustomClassification = true
+            };
+            newClassifications.Add(newClassification);
+        }
+
+        foreach (var classification in newClassifications) transaction.Classifications.Add(classification);
+        await _financeTrackerContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task RemoveCustomClassificationAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var query = _financeTrackerContext.IsolateToUser(UserId);
+
+
+        var classificationToRemove =
+            await query.Include(x => x.CustomClassifications)
+                .SelectMany(x => x.CustomClassifications)
+                .SingleAsync(x => x.Id == id, cancellationToken);
+
+
+        var transactionClassifications = await query
+            .Include(x => x.Providers)
+            .ThenInclude(x => x.Accounts)
+            .ThenInclude(x => x.Transactions)
+            .ThenInclude(x => x.Classifications)
+            .SelectMany(x =>
+                x.Providers.SelectMany(c => c.Accounts).SelectMany(r => r.Transactions)
+                    .SelectMany(x => x.Classifications))
+            .Where(x => x.IsCustomClassification == true && x.Classification == classificationToRemove.Tag)
+            .ToListAsync(cancellationToken);
+
+        await _financeTrackerContext.BulkDeleteAsync(transactionClassifications, cancellationToken: cancellationToken);
+        await _financeTrackerContext.BulkDeleteAsync([classificationToRemove], cancellationToken: cancellationToken);
+    }
+}
