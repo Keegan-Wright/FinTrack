87163d4ecdb21d11d6146024660b3686dba2efb2
Keegan Wright <keeganwright12@gmail.com>
2026-02-03 12:05:44 +0000

    Calendar service

---
+++ Src/FinanceTracker.Services/Calendar/ICalendarService.cs
@@ -0,0 +1,8 @@
+using FinanceTracker.Models.Response.Calendar;
+
+namespace FinanceTracker.Services.Calendar;
+
+public interface ICalendarService
+{
+    IAsyncEnumerable<CalendarItemsResponse> GetMonthItemsAsync(int month, int year, CancellationToken cancellationToken);
+}
