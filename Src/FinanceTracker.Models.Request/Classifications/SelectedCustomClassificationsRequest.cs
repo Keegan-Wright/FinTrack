9e4b179768e1534d5f8a92331a9ae39c89d7130a
Keegan Wright <keeganwright12@gmail.com>
2026-02-13 21:57:52 +0000

    Add classifications to transactions

--- Src/FinanceTracker.Models.Request/Classifications/SelectedCustomClassificationsRequest.cs
+++ Src/FinanceTracker.Models.Request/Classifications/SelectedCustomClassificationsRequest.cs
@@ -6,4 +6,5 @@ public class SelectedCustomClassificationsRequest
 {
     [Description("Unique identifier of the classification to apply")]
     public required Guid ClassificationId { get; set; }
+    
 }
