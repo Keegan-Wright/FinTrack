c519d2bf1692fac068cc86f77254025a7e49435f
Keegan Wright <keeganwright12@gmail.com>
2026-02-16 12:35:03 +0000

    Expansion of base report page

--- Src/FinanceTracker/Components/Pages/Reports/ReportPageBase.cs
+++ Src/FinanceTracker/Components/Pages/Reports/ReportPageBase.cs
@@ -1,15 +1,19 @@
 using FinanceTracker.Components.Transactions;
 using FinanceTracker.Enums;
+using FinanceTracker.Models.Request.Reports;
 using FinanceTracker.Models.Request.Transaction;
 using FinanceTracker.Models.Response.Transaction;
+using FinanceTracker.Services.Reports;
 using FinanceTracker.Services.Transactions;
 using Microsoft.AspNetCore.Components;
 
 namespace FinanceTracker.Components.Pages.Reports;
 
-public class ReportPageBase : PageComponent
+public class ReportPageBase<TReportResponse> : PageComponent
+
 {
     [Inject] private ITransactionsService TransactionsService { get; set; }
+    [Inject] private IReportService ReportService { get; set; }
     
     internal readonly List<TransactionAccountFilterResponse> _accountFilterItems = [];
     internal readonly List<TransactionCategoryFilterResponse> _categoryFilterItems = [];
@@ -17,14 +21,17 @@ public class ReportPageBase : PageComponent
     internal readonly List<TransactionTypeFilterResponse> _transactionTypeFilterItems = [];
     internal readonly List<TransactionTagFilterResponse> _transactionTagFilterItems = [];
     
+    private IAsyncEnumerable<TReportResponse> ReportResponseEnumerable { get; set; }
+    internal List<TReportResponse> ReportItems { get; set; }
+    
     internal readonly string _searchTerm;
     
-    internal async Task LoadReportAsync(TransactionFilters.ActiveTransactionFilters filters)
+    internal async Task LoadReportAsync(TransactionFilters.ActiveTransactionFilters filters, ReportType reportType)
     {
         SetLoadingState(true, "Loading Report");
 
         // Placeholder for reports
-        var transactionRequest = new FilteredTransactionsRequest
+        var reportRequest = new BaseReportRequest
         {
             AccountIds = filters.AccountIds.ToList(),
             ProviderIds = filters.ProviderIds.ToList(),
@@ -33,10 +40,30 @@ public class ReportPageBase : PageComponent
             SearchTerm = filters.SearchTerm,
             FromDate = filters.DateRange?.Start,
             ToDate = filters.DateRange?.End,
-            Tags = filters.TransactionTags.ToList()
+            Tags = filters.TransactionTags.ToList(),
+            SyncTypes = SyncTypes.All
         };
-        
-        // Load Report here
+
+        switch (reportType)
+        {
+            case ReportType.SpentInTimePeriod:
+                ReportResponseEnumerable = (IAsyncEnumerable<TReportResponse>)ReportService.GetSpentInTimePeriodReportAsync(reportRequest, Cts.Token);
+                break;
+            case ReportType.CategoryBreakdown:
+                ReportResponseEnumerable = (IAsyncEnumerable<TReportResponse>)ReportService.GetCategoryBreakdownReportAsync(reportRequest, Cts.Token);
+                break;
+            case ReportType.AccountBreakdown:
+                ReportResponseEnumerable = (IAsyncEnumerable<TReportResponse>)ReportService.GetAccountBreakdownReportAsync(reportRequest, Cts.Token);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(reportType), reportType, null);
+        }
+
+
+        await foreach (var reportItem in ReportResponseEnumerable.WithCancellation(Cts.Token))
+        {
+            ReportItems.Add(reportItem);
+        }
         
         SetLoadingState(false);
     }
@@ -67,5 +94,13 @@ public class ReportPageBase : PageComponent
         {
             _transactionTagFilterItems.Add(tag);
         }
+    }
+
+    protected async override Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnInitializedAsync();
+        SetLoadingState(true, "Loading filters");
+        await LoadFilterItemsAsync();
+        SetLoadingState(false);
     }
 }
