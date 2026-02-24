87475869150d20b4a0808f91f583b04acac99b03
Keegan Wright <keeganwright12@gmail.com>
2026-02-24 14:30:53 +0000

    docker setup testing

--- Src/FinanceTracker.AppHost/AppHost.cs
+++ Src/FinanceTracker.AppHost/AppHost.cs
@@ -1,31 +1,72 @@
-using FinanceTracker.AppHost;
 using Microsoft.Extensions.Configuration;
+#pragma warning disable ASPIRECOMPUTE003
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-IConfigurationSection sentryConfig = builder.Configuration.GetSection("Sentry");
+var registry = builder.AddContainerRegistry(
+    "docker-hub",                              // Registry name
+    "docker.io",                           // Registry endpoint
+    "keeganwright12/fintrack"     // Repository path
+);
+
 IConfigurationSection openBankingConfig = builder.Configuration.GetSection("OpenBanking");
-IConfigurationSection authConfig = builder.Configuration.GetSection("Auth");
 IConfigurationSection encryptionConfig = builder.Configuration.GetSection("Encryption");
 
-IResourceBuilder<RedisResource> redis = builder.AddRedis("financeTrackerRedis")
+var encryptionKey = builder.AddParameter("FinTrack-Symmetric-Key", encryptionConfig["SymmetricKey"] ?? string.Empty );
+var encryptionSalt = builder.AddParameter("FinTrack-Symmetric-Salt", encryptionConfig["SymmetricSalt"] ?? string.Empty);
+var encryptionIterations = builder.AddParameter("FinTrack-Symmetric-Key-Iterations",encryptionConfig["Iterations"] ?? string.Empty);
+
+var openBankingAuthUrl = builder.AddParameter("OpenBanking-Auth-Url", openBankingConfig["TrueLayer:BaseAuthUrl"] ?? string.Empty);
+var openBankingDataUrl = builder.AddParameter("OpenBanking-Data-Url", openBankingConfig["TrueLayer:BaseDataUrl"] ?? string.Empty);
+var openBankingAuthRedirectUrl = builder.AddParameter("OpenBanking-Auth-Redirect-Url", openBankingConfig["TrueLayer:AuthRedirectUrl"] ?? string.Empty);
+var openBankingClientId = builder.AddParameter("OpenBanking-Client-Id", openBankingConfig["TrueLayer:ClientId"] ?? string.Empty);
+var openBankingClientSecret = builder.AddParameter("OpenBanking-Client-Secret", openBankingConfig["TrueLayer:ClientSecret"] ?? string.Empty);
+
+builder.AddDockerComposeEnvironment("compose")
+    .WithDashboard(dashboard =>
+    {
+        dashboard.WithContainerName("FinTrack-Dashboard");
+        dashboard.WithHostPort(8080)
+            .WithForwardedHeaders();
+    });
+
+IResourceBuilder<RedisResource> redis = builder.AddRedis("FinTrack-Redis")
     .WithDataVolume(isReadOnly: false)
-    .WithPersistence(TimeSpan.FromMinutes(5));
+    .WithPersistence(TimeSpan.FromMinutes(5))
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "FinTrack-Redis";
+    });
 
-IResourceBuilder<PostgresServerResource> postgres = builder.AddPostgres("financeTrackerPostgres")
+IResourceBuilder<PostgresServerResource> postgres = builder.AddPostgres("FinTrack-Postgres")
     .WithPgWeb()
-    .WithDataVolume(isReadOnly: false);
+    .WithDataVolume(isReadOnly: false)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "FinTrack-Postgres";
+    });
 
-IResourceBuilder<PostgresDatabaseResource> postgresDb = postgres.AddDatabase("financeTrackerPostgresDb");
+IResourceBuilder<PostgresDatabaseResource> postgresDb = postgres.AddDatabase("FinTrackDb");
 
-builder.AddProject<Projects.FinanceTracker>("FinanceTracker")
-    .AddSentry(sentryConfig)
-    .AddAuth(authConfig)
-    .AddEncryption(encryptionConfig)
-    .AddOpenBanking(openBankingConfig)
+#pragma warning disable ASPIREPIPELINES003
+builder.AddProject<Projects.FinanceTracker>("FinTrack")
+
+    .WithEnvironment("ENCRYPTION_KEY", encryptionKey)
+    .WithEnvironment("ENCRYPTION_SALT", encryptionSalt)
+    .WithEnvironment("ENCRYPTION_ITERATIONS", encryptionIterations)
+    .WithEnvironment("OPEN_BANKING_TRUELAYER_BASE_AUTH_URL", openBankingAuthUrl)
+    .WithEnvironment("OPEN_BANKING_TRUELAYER_BASE_DATA_URL", openBankingDataUrl)
+    .WithEnvironment("OPEN_BANKING_TRUELAYER_AUTH_REDIRECT_URL", openBankingAuthRedirectUrl)
+    .WithEnvironment("OPEN_BANKING_TRUELAYER_CLIENT_ID", openBankingClientId)
+    .WithEnvironment("OPEN_BANKING_TRUELAYER_CLIENT_SECRET", openBankingClientSecret)
     .WithReference(redis)
-    .WaitFor(redis)
     .WithReference(postgresDb)
-    .WaitFor(postgresDb);
+    .WaitFor(redis)
+    .WaitFor(postgresDb)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "FinTrack";
+    })
+    .WithContainerRegistry(registry);
 
 await builder.Build().RunAsync();
